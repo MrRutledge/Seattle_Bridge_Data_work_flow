e7bd4d2fa03f56ef520223dbc580ca1e278d4a33
MrRutledge <rahimrutledge@gmail.com>
2018-08-07 23:49:09 +0100

    updated the main notebook

--- jupyterworkflow/tests/test_data.py
+++ jupyterworkflow/tests/test_data.py
@@ -1,9 +1,11 @@
 from jupyterworkflow.data import get_fremont_data
 import pandas as pd
+import numpy as np
 
 def test_f_data():
      data = get_fremont_data()
      assert all(data.columns == ['West', 'East', 'Total'])
      assert isinstance(data.index, pd.DatetimeIndex)
+     assert len(np.unique(data.index.time)==24)
 
 
